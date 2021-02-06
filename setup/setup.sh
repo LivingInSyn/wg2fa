3e469b2b38665eaec3c3e623f38210cd9b0af036
Jeremy Mill <jeremymill@gmail.com>
2021-02-06 09:11:50 -0500

    update developer build shellscript

--- setup/setup.sh
+++ setup/setup.sh
@@ -1,3 +1,7 @@
+# download go and put it on the path
+wget https://golang.org/dl/go1.15.8.linux-amd64.tar.gz
+tar -C /usr/local -xzf go1.15.8.linux-amd64.tar.gz
+export PATH=$PATH:/usr/local/go/bin
 # install wireguard
 sudo apt install wireguard
 # build the server public and private keys
@@ -7,10 +11,21 @@ cp ./wg0.conf /etc/wireguard/
 sudo sed -i "s/SERVER_PRIVATE_KEY/$(cat /etc/wireguard/privatekey)/g" /etc/wireguard/wg0.conf
 # update permissions
 sudo chmod 600 /etc/wireguard/privatekey
-sudo chmod 600 /etc/wireguard/wg0.conf
-# create wg2fa dirs
-#sudo mkdir /etc/wireguard/clientConfigs
-#sudo mkdir /etc/wireguard/clientList
+sudo chmod 600 /etc/wireguard/wg0.config
+
 #startup with systemd (un comment)
+sudo wg-quick up wg0
 #sudo systemctl enable wg-quick@wg0
 #sudo systemctl start wg-quick@wg0
+
+# download and build wg2fa
+wget https://github.com/LivingInSyn/wg2fa/archive/main.zip
+unzip main.zip
+cd wg2fa-main
+go build -o wg2fa
+# start wg2fa
+sudo ./wg2fa --wgc /etc/wireguard/wg0.conf --cl /etc/wireguard/clientDB \
+--dangerauth \
+--debug \
+--cid a \
+--iss b
